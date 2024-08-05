24b745c3c46897c1bba77e8412b89146931f9aa8
DheerajPKumar <dheeraj.kumar@axeno.co>
2024-08-05 21:46:07 +0530

    Creating functionality for Assignment-1

---
+++ script1.js
@@ -0,0 +1,37 @@
+const inputEl = document.getElementById('container__input');
+const btn = document.getElementById('container__btn');
+const list = document.getElementById('container__list');
+
+function create(tag) {
+    const chipEl = document.createElement('div');
+    chipEl.classList.add('chip');
+
+    const myTag = document.createElement('p');
+    myTag.textContent = tag;
+    chipEl.appendChild(myTag);
+
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'x';
+    removeBtn.classList.add('removeBtn');
+
+    removeBtn.addEventListener('click', func);
+
+    function func() {
+        chipEl.parentNode.removeChild(chipEl);
+    }
+
+    chipEl.appendChild(removeBtn);
+    return chipEl;
+}
+
+function addTag() {
+    const tag = inputEl.value;
+    if (tag) {
+        const chipEl = create(tag);
+        list.appendChild(chipEl);
+        inputEl.value = '';
+        inputEl.focus();
+    }
+}
+
+btn.addEventListener('click', addTag);
