f8127adaf5bfa4b2ac3a54b0d6ad4ce9532f49a0
DheerajPKumar <dheeraj.kumar@axeno.co>
2024-08-06 13:23:32 +0530

    Assignment-2 completed

--- script2.js
+++ script2.js
@@ -1,15 +1,39 @@
 const words = [
-    "apple", "applet", "appreciate", "banana", "bananas", "bannister", "cat", "catalog", "cater", 
-    "dog", "dheeraj", "dogma", "dogged", "elephant", "elevate", "elevator", "fish", "fishing", "fished",
-    "grape", "grapes", "grapefruit", "house", "housetop", "housing", "ice", "iceberg", "icy", 
-    "jacket", "jack", "jacking", "kite", "kitchen", "kitten", "lemon", "lemons", "lemonade"
+    "apple", "applet", "appreciate", "applaud", "applause", "appoint", "appointment", "apportion", 
+    "apposite", "appreciate", "apprehend", "apprehensive", "approach", "appropriate",
+    "approve", "banana", "bananas", "bannister", "bald", "ball", "ballad", "ballerina",
+    "balloon", "ballot", "bamboo", "ban", "band", "bandage", "cat", "catalog", "cater", 
+    "cage", "cake", "calculate", "calendar", "calibrate", "call", "calm", "doll",
+    "dollar", "dolphin", "domain", "domestic", "dominate", "donate",
+    "dog", "dogma", "dogged", "elephant", "elevate", "elevator", "elephant", "elevate",
+    "elevation", "elevator", "eleven", "eligible", "eliminate", "elite", "elongate", "else","fish", 
+    "fishing", "fished", "grape", "grapes", "grapefruit", "house", "housetop", "housing", "ice", 
+    "iceberg", "icy", "jacket", "jack", "jacking", "kite", "kitchen", "kitten", "lemon", "lemons", "lemonade"
 ];
 
 const search = document.getElementsByClassName('container__search')[0];
 const containerList = document.getElementsByClassName('list')[0];
 
 function func() {
-    const searchedWord = search.value.toLowerCase();
+    const searchedWord = search.value.toLowerCase().trim();
+    containerList.innerText = '';
+
+    if (searchedWord.length >= 1) {
+        const suggestedWords = words.filter(word => word.toLowerCase().includes(searchedWord));
+        suggestedWords.forEach((word) => {
+            const li = document.createElement('li');
+            li.textContent = word;
+            li.classList.add('individualList');
+
+            function addFunc() {
+                search.value = word;
+                containerList.innerText = '';
+            }
+
+            li.addEventListener('click', addFunc);
+            containerList.appendChild(li);
+        });
+    }
 }
 
 search.addEventListener("input", func);
